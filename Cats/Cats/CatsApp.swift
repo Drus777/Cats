0edba0802b506ba317b1eaa701306a0db582f8e3
Andrey Krivlenya <>
2021-11-07 13:46:58 +0300

    init

---
+++ Cats/Cats/CatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatsApp.swift
+//  Cats
+//
+//  Created by Andrey on 7.11.21.
+//
+
+import SwiftUI
+
+@main
+struct CatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
